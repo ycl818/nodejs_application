cfb1681687c89d262f2fb1cffc4f665c003e09ff
yichien <kevinyichienlee@gmail.com>
2024-02-28 00:10:11 +0800

    setup eslint + prettier

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
-
-console.log(process.env);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
