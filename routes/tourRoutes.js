bd9a9287a0ba052254b29a9256ee69b3b513f1aa
yichien <kevinyichienlee@gmail.com>
2024-02-27 23:19:51 +0800

    chaining multiple middleware functions

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,10 +6,15 @@ const router = express.Router();
 
 router.param('id', tourController.checkID);
 
+// Create a checkBodt middleware
+// check if body contains the name and price property
+// if not, send back 400 (bad request)
+// add it to the post handler stack
+
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.createTour);
+  .post(tourController.checkBody, tourController.createTour);
 
 router
   .route('/:id')
