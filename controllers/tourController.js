3700d9aad4c3101d63c917212a53824759229486
yichien <kevinyichienlee@gmail.com>
2024-02-28 22:07:29 +0800

    API sorting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,25 +3,38 @@ const Tour = require('../models/tourModel');
 exports.getAllTours = async (req, res) => {
   try {
     // BUILD QUERY
+    // 1A) Filtering
     const queryObj = { ...req.query };
     const excludeFields = ['page', 'sort', 'limit', 'fields'];
     excludeFields.forEach((el) => delete queryObj[el]);
 
-    console.log('adjust: ', queryObj);
+    // 1B) Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
     // const tours = await Tour.find({
     //   duration: 5,
     //   difficulty: 'easy',
     // });
+
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    // EXECUTE QUERY
+    const tours = await query;
 
     // const tours = await Tour.find()
     //   .where('duration')
     //   .equals(5)
     //   .where('difficulty')
     //   .equals('easy');
-    const query = Tour.find(queryObj);
-
-    // EXECUTE QUERY
-    const tours = await query;
 
     res.status(200).json({
       status: 'success',
